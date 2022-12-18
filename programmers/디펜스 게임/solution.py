900e076f7a97799c2f4d0027f999e4926452500e
Geun Seong Lee <79559361+LEEGURTS@users.noreply.github.com>
2022-12-19 04:54:37 +0900

    uploaded at 2022. 12. 19. 오전 4:54:36

---
+++ programmers/디펜스 게임/solution.py
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/142085?language=python3
+
+def solution(n, k, enemy):
+    def isPossible(mid):
+        sortedArr = sorted(enemy[:mid+1])[::-1]
+        if sum(enemy[:mid+1])-sum(sortedArr[:k])<=n:
+            return True
+        else:
+            return False
+        
+    left=0
+    right=len(enemy)
+    while left<right:
+        mid = (left+right)//2
+        if isPossible(mid):
+            left = mid+1
+        else:
+            right = mid
+    return left
